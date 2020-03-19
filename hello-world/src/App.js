ba423a1ccf33d5a24340c2158d63606543397448
Daniel <daniell-henrique@hotmail.com>
2020-03-19 19:46:12 -0300

    Basics of Form Handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,71 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBind from './components/EventBind'
-import ParentComponent from './components/ParentComponent'
-import UserGreeting from './components/UserGreeting'
-import NameList from './components/NameList'
-import Stylesheet from './components/Stylesheet'
-import Inline from './components/Inline';
+import Form from './components/Form'
 
-import './appStyles.css'
-import styles from './appStyles.module.css'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-
-      <h1 className='error'>Error</h1>
-
-      <h1 className={styles.success}>Success</h1>
-
-      {/* <Stylesheet primary={true}/> */}
-
-      {/* <Inline/> */}
-
-      {/* <NameList/> */}
-
-      {/* <UserGreeting/> */}
-
-      {/* <ParentComponent/> */}
-
-      {/* <EventBind/> */}
-
-      {/* <FunctionClick/> */}
-      
-      {/* <ClassClick/> */}
-
-      {/* <Counter/> */}
-
-      {/* <Message/> */}
-
-      {/* <Greet name="Daniel" profession="Developer">
-        <p>This is children props</p>
-      </Greet>
-      
-      <Greet name="Maria" profession="Software Engineer">
-        <button>Click</button>
-      </Greet>
-
-      <Greet name="Carlos" profession="UI Designer"/>
-
-      <Welcome name="Daniel" profession="Developer"/>
-      <Welcome name="Maria" profession="Software Engineer"/>
-      <Welcome name="Carlos" profession="UI Designer"/> */}
-
-      {/* <Greet name="Daniel" profession="Developer"/> */}
-
-      {/* <Welcome name="Daniel" profession="Developer"/> */}
-
-    </div>
-  )
+    render(){
+        return(
+            <div className="App">
+                <Form/>
+            </div>
+        )
+    }
 }
 
 export default App
---
+++ hello-world/src/components/Form.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             username: '',
+             comments: '',
+             topic: 'react'
+        }
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleCommentsChange = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
+
+    handleTopicChange = (event) => {
+        this.setState({
+            topic: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+        event.preventDefault()
+    }
+    
+    render() {
+        const {username, comments, topic} = this.state
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>Username: </label>
+                    <input 
+                        type='text' 
+                        value={username} 
+                        onChange={this.handleUsernameChange}/>
+                </div>
+                <br/>
+                <div>
+                    <label>Comments: </label>
+                    <textarea 
+                        value={comments} 
+                        onChange={this.handleCommentsChange} 
+                    />
+                </div>
+                <br/>
+                <div>
+                    <label>Topic: </label>
+                    <select value={topic} onChange={this.handleTopicChange}>
+                        <option value="react">React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+                    </select>
+                </div>
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+}
+
+export default Form
