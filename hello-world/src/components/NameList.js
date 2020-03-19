efd5ce16b4ff8eeb8edddc0907d91a5fc5be9e12
Daniel <daniell-henrique@hotmail.com>
2020-03-19 17:16:26 -0300

    Index as Key

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -3,6 +3,8 @@ import Person from './Person'
 
 function NameList() {
 
+    const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
+
     const persons = [
         {
             id: 1,
@@ -24,9 +26,11 @@ function NameList() {
         }
     ]
 
-    const personList = persons.map(person => <Person person={person}/>)
+    // const personList = persons.map(person => <Person key={person.id} person={person}/>)
 
-    return (<div>{personList}</div>)
+    const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+
+    return (<div>{nameList}</div>)
 }
 
 export default NameList
