a202d3a2e5e3c34cd656b807a636e6d314d3ce24
Daniel <daniell-henrique@hotmail.com>
2020-03-19 20:07:46 -0300

    Component Mounting Lifecycle Methods

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
-import Form from './components/Form'
+import LifecycleA from './components/LifecycleA';
 
 class App extends Component {
 
     render(){
         return(
             <div className="App">
-                <Form/>
+                <LifecycleA/>
             </div>
         )
     }
---
+++ hello-world/src/components/LifecycleA.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import LifecycleB from './LifecycleB'
+
+class LifecycleA extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: 'Daniel'
+        }
+        console.log('LifecycleA @ constructor')
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('LifecycleA @ getDerivedStateFromProps')
+        return null
+    }
+
+    componentDidMount() {
+        console.log('LifecycleA @ componentDidMount')
+    }
+
+    render() {
+        console.log('LifecycleA @ render')
+        return (
+            <div>
+                <div>LifecycleA</div>
+                <LifecycleB />
+            </div>
+
+        )
+    }
+}
+
+export default LifecycleA
