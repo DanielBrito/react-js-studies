494d8dd688568b88fe6ef9ef588e6432f3c2c8b7
Daniel <daniell-henrique@hotmail.com>
2020-03-19 10:42:41 -0300

    Functional Components

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/*
+function Greet(){
+    return <h1>Hello, Daniel!</h1>
+}
+*/
+
+// Rewriting with ES6 syntax:
+
+const Greet = () => <h1>Hello, Daniel!</h1>
+
+export default Greet;
