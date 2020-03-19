a515f818ddc5a41cbb46f77e182c0d33c01ec703
Daniel <daniell-henrique@hotmail.com>
2020-03-19 11:48:42 -0300

    JSX

---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hello = () => {
+
+    // JSX Example:
+    // return(
+    //     <div className='exampleClass'>
+    //         <h1>Hello, Daniel!</h1>
+    //         <h3>This is a JSX Example</h3>
+    //     </div>
+    // )
+
+    // Without JSX Example:
+    return React.createElement(
+        'div', 
+        {id: 'hello', className: 'exampleClass'}, 
+        React.createElement('h1', null, 'Hello, Daniel!')
+    );
+}
+
+export default Hello;
