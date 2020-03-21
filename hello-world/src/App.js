ebed773428b9657b7682047cd82873e3d613d0aa
Daniel <daniell-henrique@hotmail.com>
2020-03-21 17:20:57 -0300

    Error Boundary

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react'
 import './App.css';
-import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
 
 class App extends Component {
 
     render(){
         return(
             <div className="App">
-                <PortalDemo/>
+                <ErrorBoundary>
+                    <Hero heroName="Batman"/>
+                </ErrorBoundary>
+
+                <ErrorBoundary>
+                    <Hero heroName="Superman"/>
+                </ErrorBoundary>
+
+                <ErrorBoundary>
+                    <Hero heroName="Joker"/>
+                </ErrorBoundary>
             </div>
         )
     }
---
+++ hello-world/src/components/ErrorBoundary.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             hasError: false
+        }
+    }
+    
+    static getDerivedStateFromError(error){
+        return {
+            hasError: true
+        }
+    }
+
+    componentDidCatch(error, info){
+        console.log(error)
+        console.log(info)
+    }
+
+    render() {
+        if(this.state.hasError){
+            return <h1 style={{color: "red"}}>Something went wrong!</h1>
+        }
+
+        return this.props.children
+    }
+}
+
+export default ErrorBoundary
