aad85740f7b82432731da6bc0abcdb3ae5fb7751
Daniel <daniell-henrique@hotmail.com>
2020-03-21 16:13:10 -0300

    Refs with Class Compoments

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import './App.css';
-import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
 
 class App extends Component {
 
     render(){
         return(
             <div className="App">
-                <RefsDemo/>
+                <FocusInput/>
             </div>
         )
     }
