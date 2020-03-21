b10a94e1cc9cb59694bd57b3ac1e20d924b70861
Daniel <daniell-henrique@hotmail.com>
2020-03-21 16:53:09 -0300

    Portals

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import './App.css';
-import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
 
 class App extends Component {
 
     render(){
         return(
             <div className="App">
-                <FRParentInput/>
+                <PortalDemo/>
             </div>
         )
     }
