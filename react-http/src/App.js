d8185df8463995033d559901d742478c8246e8c9
Daniel <daniell-henrique@hotmail.com>
2020-03-22 01:24:41 -0300

    HTTP Post Request

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
     return (
         <div className="App">
-            <PostList />
+            <PostForm/>
         </div>
     );
 }
