abbcbd15c0361c5d091b61b52bb532821636477e
Daniel <daniell-henrique@hotmail.com>
2020-08-09 15:27:46 -0300

    Configuration

--- react-hooks-firebase/src/config/firebase.js
+++ react-hooks-firebase/src/config/firebase.js
@@ -1,15 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/firebase-firestore";
 
+// Set your configuration:
 var firebaseConfig = {
-  apiKey: "AIzaSyDSk_I7uQRG73f3F1ugC2e52cVzdj3QZss",
-  authDomain: "clock-it-9a34e.firebaseapp.com",
-  databaseURL: "https://clock-it-9a34e.firebaseio.com",
-  projectId: "clock-it-9a34e",
-  storageBucket: "clock-it-9a34e.appspot.com",
-  messagingSenderId: "11506270470",
-  appId: "1:11506270470:web:04d94ad08335a62b8b9e87",
-  measurementId: "G-T1L5RYP9LJ",
+  apiKey: "..."
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "...",
 };
 
 firebase.initializeApp(firebaseConfig);
